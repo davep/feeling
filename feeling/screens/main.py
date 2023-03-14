ad7edeb253c4e66f8ef1344ee15c45e8f827280b
Dave Pearson <davep@davep.org>
2023-03-14 21:52:08 +0000

    :lipstick: Experimenting with a better look
    The tree was just to see if the data was working. I'm happy that it is but
    wasn't happy with the UI. This... I'm unsure yet. I'll keep plugging away
    with this idea and see how I feel.

--- feeling/screens/main.py
+++ feeling/screens/main.py
@@ -2,38 +2,58 @@
 
 ##############################################################################
 # Textual imports.
-from textual.app     import ComposeResult
-from textual.screen  import Screen
-from textual.widgets import Header, Footer, Tree
-from textual.binding import Binding
+from textual.app        import ComposeResult
+from textual.screen     import Screen
+from textual.containers import Horizontal
+from textual.widgets    import Header, Footer, ListView, ListItem, Label
+from textual.binding    import Binding
 
 ##############################################################################
 # Rich imports.
-from rich.emoji import Emoji
+from rich.text import Text
 
 ##############################################################################
 # Local imports.
-from ..data import load, Scale
+from ..data import load, Scale, Feelings
 
 ##############################################################################
-# The main screen.
-class Main( Screen ):
-    """The main screen for the application."""
+class FeelingItem( ListItem ):
+    """Base class for a feeling list item."""
 
-    BINDINGS = [
-        Binding( "escape", "app.quit", "Quit" ),
-    ]
-    """The bindings for the main screen."""
+    DEFAULT_CSS = """
+    FeelingItem {
+        padding: 1;
+    }
 
-    def compose( self ) -> ComposeResult:
-        """Compose the screen.
+    FeelingItem > Label {
+        width: 100%;
+        color: black;
+    }
 
-        Returns:
-            The composed widgets.
-        """
-        yield Header()
-        yield Tree( "Feelings" )
-        yield Footer()
+    FeelingItem > Label.very_low {
+        background: red 80%;
+    }
+
+    FeelingItem > Label.low {
+        background: red 30%;
+    }
+
+    FeelingItem > Label.neutral {
+        background: green 60%;
+    }
+
+    FeelingItem > Label.good {
+        background: green 80%;
+    }
+
+    FeelingItem > Label.very_good {
+        background: green;
+    }
+    """
+
+    def __init__( self, feelings: Feelings ) -> None:
+        super().__init__()
+        self._feelings = feelings
 
     @staticmethod
     def emoji( scale: Scale ) -> str:
@@ -53,48 +73,149 @@ class Main( Screen ):
             Scale.VERY_LOW: ":frowning:"
         }[ scale ]
 
-    @staticmethod
-    def colour( scale: Scale ) -> str:
-        """Get a colour for the label given a scale.
+##############################################################################
+class Day( FeelingItem ):
+    """A list item that's a day's feeling information."""
 
-        Args:
-            scale: The scale to get the colour for.
+    def __init__( self, feelings: Feelings, year: str, month: str, day: str ) -> None:
+        super().__init__( feelings )
+        self._year        = year
+        self._month       = month
+        self._day         = day
+
+    def compose( self ) -> ComposeResult:
+        """Compose the child widgets."""
+        scale = self._feelings.day_scale( self._year, self._month, self._day )
+        yield Label(
+            Text.from_markup( f"{self.emoji( scale )} {self._year}-{self._month}-{self._day}" ),
+            classes=scale.name.lower()
+        )
+
+##############################################################################
+class Month( FeelingItem ):
+    """A list item that's a month's feeling information."""
+
+    def __init__( self, feelings: Feelings, year: str, month: str ) -> None:
+        super().__init__( feelings )
+        self._year  = year
+        self._month = month
+
+    @property
+    def days( self ) -> list[ Day ]:
+        """The list of recorded days in this month."""
+        return [
+            Day( self._feelings, self._year, self._month, day )
+            for day in reversed( self._feelings.days( self._year, self._month ) )
+        ]
+
+    def compose( self ) -> ComposeResult:
+        """Compose the child widgets."""
+        scale = self._feelings.month_scale( self._year, self._month )
+        yield Label(
+            Text.from_markup( f"{self.emoji( scale )} {self._year}-{self._month}" ),
+            classes=scale.name.lower()
+        )
+
+##############################################################################
+class Year( FeelingItem ):
+    """A list item that a year's feeling information."""
+
+    def __init__( self, feelings: Feelings, year: str ) -> None:
+        super().__init__( feelings )
+        self._year = year
+
+    @property
+    def months( self ) -> list[ Month ]:
+        """The list of recorded months in this year."""
+        return [
+            Month( self._feelings, self._year, month )
+            for month in reversed( self._feelings.months( self._year ) )
+        ]
+
+    def compose( self ) -> ComposeResult:
+        """Compose the child widgets."""
+        scale = self._feelings.year_scale( self._year )
+        yield Label(
+            Text.from_markup( f"{self.emoji( scale )} {self._year}" ),
+            classes=scale.name.lower()
+        )
+
+##############################################################################
+# The main screen.
+class Main( Screen ):
+    """The main screen for the application."""
+
+    BINDINGS = [
+        Binding( "escape", "app.quit", "Quit" ),
+    ]
+    """The bindings for the main screen."""
+
+    DEFAULT_CSS = """
+    ListView {
+        width: 1fr;
+        background: $panel;
+        border: round $primary;
+    }
+    """
+
+    def compose( self ) -> ComposeResult:
+        """Compose the screen.
 
         Returns:
-            A colour label for Rich.
+            The composed widgets.
         """
-        return {
-            Scale.VERY_GOOD: "[black on spring_green2]",
-            Scale.GOOD: "[black on green3]",
-            Scale.NEUTRAL: "[black on green]",
-            Scale.LOW: "[black on orange_red1]",
-            Scale.VERY_LOW: "[white on red]"
-        }[ scale ]
+        # pylint:disable=attribute-defined-outside-init
+        yield Header()
+        with Horizontal():
+            with ListView( id="years" ) as years:
+                self.years = years # pylint:disable=attribute-defined-outside-init
+            with ListView( id="months" ) as months:
+                self.months = months# pylint:disable=attribute-defined-outside-init
+            with ListView( id="days" ) as days:
+                self.days = days # pylint:disable=attribute-defined-outside-init
+            with ListView( id="feelings" ) as feelings:
+                self.feelings = feelings # pylint:disable=attribute-defined-outside-init
+        yield Footer()
 
-    def on_mount( self ) -> None:
+    async def on_mount( self ) -> None:
         """Populate the display once the DOM is mounted."""
-        tree           = self.query_one( Tree )
-        tree.show_root = False
-        feelings       = load()
-        for year in reversed( feelings.years() ):
-            year_node = tree.root.add(
-                f"{self.colour( feelings.year_scale( year ) )}{year} {self.emoji( feelings.year_scale( year ) )}",
-                expand=True
-            )
-            for month in reversed( feelings.months( year ) ):
-                month_node = year_node.add(
-                    f"{self.colour( feelings.month_scale( year, month ) )}"
-                    f"{month} {self.emoji( feelings.month_scale( year, month ) )}",
-                    expand=True
-                )
-                for day in reversed( feelings.days( year, month ) ):
-                    day_node = month_node.add(
-                        f"{self.colour( feelings.day_scale( year, month, day ) )}"
-                        f"{day} {self.emoji( feelings.day_scale( year, month, day ) )}"
-                    )
-                    for feeling in reversed( list( feelings.for_day( year, month, day ) ) ):
-                        day_node.add_leaf( Emoji.replace(
-                            f"{self.colour( feeling.feeling )}{self.emoji( feeling.feeling )} {feeling.description}"
-                        ) )
+        self.data = load() # pylint:disable=attribute-defined-outside-init
+        for year in reversed( self.data.years() ):
+            await self.years.append( Year( self.data, year ) )
+
+    async def show_year( self, year: ListItem | None ) -> None:
+        """Show the data for the given year.
+
+        Args:
+            year: The year to show the data for, or `None` if no year active.
+        """
+        await self.months.clear()
+        if year is not None:
+            assert isinstance( year, Year )
+            for month in year.months:
+                await self.months.append( month )
+
+    async def show_month( self, month: ListItem | None ) -> None:
+        """Show the data for the given month.
+
+        Args:
+            month: The month to show the data for, or `None` if no month active.
+        """
+        await self.days.clear()
+        if month is not None:
+            assert isinstance( month, Month )
+            for day in month.days:
+                await self.days.append( day )
+
+    async def on_list_view_highlighted( self, event: ListView.Highlighted ) -> None:
+        """Handle list view highlight events.
+
+        Args:
+            event: The ListView highlight event to handle.
+        """
+        if event.list_view.id == "years":
+            await self.show_year( event.item )
+        elif event.list_view.id == "months":
+            await self.show_month( event.item )
 
 ### main.py ends here
