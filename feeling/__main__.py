0b6ef8d8db90e3b79c4f8124c77413d4c24608fb
Dave Pearson <davep@davep.org>
2023-03-16 21:07:12 +0000

    :hammer: Make the main entry point easier to get at

--- feeling/__main__.py
+++ feeling/__main__.py
@@ -5,11 +5,16 @@
 from . import cli, chui
 
 ##############################################################################
-# Run the app if we're being called as the main entry point.
-if __name__ == "__main__":
+def main() -> None:
+    """Main entry point."""
     # If the CLI didn't handle this invocation...
     if not cli.run():
         # ...go with the full CHUI.
         chui.run()
+
+##############################################################################
+# Run the app if we're being called as the main entry point.
+if __name__ == "__main__":
+    main()
 
 ### __main__.py ends here
